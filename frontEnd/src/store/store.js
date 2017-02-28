5ca9512f9953714ebb195d1d794d56dd12e9fb97
WYseven <consolelog@126.com>
2017-03-01 06:43:55 +0800

    修改路由默认值为info

--- frontEnd/src/store/store.js
+++ frontEnd/src/store/store.js
@@ -8,12 +8,13 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
+  strict: true,
   state: {
     currentNav: 'info'
   },
   mutations: {
     currentNavChange (state,a) {
-      state.currentNav = a;
+      state.currentNav = a || 'info';
     }
   }
 })
